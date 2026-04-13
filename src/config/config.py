ca8a634a86280b51b38d3f84a99e7f1b2ccbe8bb
Siddharth <siddharth.arum@gmail.com>
2026-04-13 10:36:10 +0000

    troubleshoot issues in logging

--- src/config/config.py
+++ src/config/config.py
@@ -11,7 +11,7 @@ my_config = MyConfig()
 @dataclass
 class DataConfig:
     data_path: str = "src/data/tiny-imagenet-fixed"
-    gpu_batch_size: int = 1
+    gpu_batch_size: int = 64
     num_workers: int = 4
     pin_memory: bool = True
 
@@ -19,8 +19,8 @@ data_config = DataConfig()
 
 @dataclass
 class HyperParamConfig:
-    total_batch_size: int = 1
-    total_train_steps: int = int(1e5)
+    total_batch_size: int = 128
+    total_train_steps: int = 80000
     warmup_fraction: float = 0.1
     max_fraction: float = 0.8
     min_lr: float = 1e-3
@@ -37,9 +37,9 @@ class ModelConfig:
     patch_size: int = 4
     in_chans: int = 3
     embed_dim: int = 96
-    depths: list = field(default_factory=lambda: [1,1,1,1])
-    num_heads: list = field(default_factory=lambda: [3,3,6,6])# [3, 6, 12, 24])
-    global_num_heads: list = field(default_factory=lambda: [3,3,6,6])#[3, 6, 12, 24])
+    depths: list = field(default_factory=lambda: [2,2,6,2])
+    num_heads: list = field(default_factory=lambda: [3, 6, 12, 24])
+    global_num_heads: list = field(default_factory=lambda: [3, 6, 12, 24])
     window_size: int = 7
     mlp_ratio: float = 4.
     qkv_bias: bool = True
@@ -57,14 +57,14 @@ model_config = ModelConfig()
 @dataclass
 class RunConfig():
     round: str = "initial"
-    iter_id: str = "001"
+    iter_id: str = "002"
     MODEL_FILE_PATH: str = None
     LOG_FILE_PATH: str = None
     model_save_interval: int = int(1e4)
     num_classes: int = 200
     val_interval: int = 10000
     model_version: str = "v1"
-    dry_run: bool = True
+    dry_run: bool = False
     
 
 run_config = RunConfig()
